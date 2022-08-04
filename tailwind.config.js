91cd2063b3e726db9128c90de4a051227c53d301
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-08-05 01:01:24 +0200

    customize taillwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/**/*.{js,ts,jsx,tsx}"],
-  content: [],
   theme: {
+    screens: {
+      sm: '460px',
+      md: '768px',
+      lg: '1024px',
+    },
+    colors: ({ colors }) => ({
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      emerald: colors.emerald,
+    }),
     extend: {},
   },
   plugins: [],
-}
+};
