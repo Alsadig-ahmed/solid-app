3a02273a6a30f5ca9e18566775c7c488675736c2
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-08-05 01:04:45 +0200

    run formating

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,10 @@ function App() {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <h1 class="w-[10rem]  h-[10rem] text-center  rounded-[50%] mb-4 p-8 bg-slate-400 ">hello world {gh()}</h1>
-        <button class="bg-slate-600 px-2 rounded-lg hover:bg-slate-200 hover:text-emerald-300 active:bg-zinc-600" onClick={() => ghSet(8)}>
+        <h1 class="w-[10rem]  h-[10rem] text-center  rounded-[50%] mb-4 p-8 bg-slate-400 ">
+          hello world {gh()}
+        </h1>
+        <button class="btn" onClick={() => ghSet(8)}>
           change
         </button>
       </header>
